e39fdaa915578830ee298e575dc1c6d46dc9e9c1
djomla <mladen.martincevic@spilgames.com>
2013-03-22 11:31:32 +0100

    Fixing setup

--- setup-buster.sh
+++ setup-buster.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-result=${PWD}
-export PATH=$PATH:$result
+current_path=${PWD}
+buster_path=$current_path'/npm/buster/'
+nodejs_path=$current_path'/node/bin/'
+conf_file='.conf'
+
+
+if [ ! -f $conf_file ];
+then
+	echo 'PATH='$PATH:$buster_path:$nodejs_path >> ~/.profile
+	touch .conf
+	source ~/.profile
+fi
 
